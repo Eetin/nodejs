7422f795dc70cf4d3a01d34bf07650dbb4ae04a7
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-29 13:35:02 +0000

    10. Time Server

--- program.js
+++ program.js
@@ -1,17 +1,8 @@
 "use strict";
-var http = require('http');
-var bl = require('bl');
+const net = require('net')
+const strftime = require('strftime')
 
-var strings = [];
-var count = 0;
-
-for (let i = 0; i < 3; i++) {
-    http.get(process.argv[i+2], function(response) {
-        response.pipe(bl(function(err, data) {
-            if (err) return console.error(err);
-            strings[i] = data.toString();
-            count++;
-            if (count == 3) strings.forEach(string => console.log(string));
-        }));
-    }).on('error', console.error);
-}
+const server = net.createServer(socket => {
+    socket.end(`${strftime('%Y-%m-%d %H:%M')}\n`);
+})
+server.listen(+process.argv[2]);
