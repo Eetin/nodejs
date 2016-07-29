1b9b0ce181ce0bd49998ba134f6a0f5b02dd3940
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-29 14:17:51 +0000

    13. HTTP JSON Api Server

--- program.js
+++ program.js
@@ -1,9 +1,27 @@
 "use strict";
 const http = require('http')
-const map = require('through2-map')
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-    if (req.method != 'POST') return res.end('send me a POST\n')
-    req.pipe(map(chunk => chunk.toString().toUpperCase())).pipe(res)
+    const parsedUrl = url.parse(req.url, true)
+    let jsonResponse = undefined
+    if (parsedUrl.pathname === '/api/parsetime') {
+        const date = new Date(parsedUrl.query.iso)
+        jsonResponse = {
+            hour: date.getHours(),
+            minute: date.getMinutes(),
+            second: date.getSeconds()
+        }
+    } else if (parsedUrl.pathname === '/api/unixtime') {
+        jsonResponse = { unixtime: new Date(parsedUrl.query.iso).getTime() }
+    }
+    if (jsonResponse) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(jsonResponse))
+    }
+    else {
+        res.writeHead(404);
+        res.end();
+    }
 })
 server.listen(+process.argv[2]);
