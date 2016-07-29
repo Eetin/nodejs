bf2a08bd910b6478a4193547a35764f1d3e097ed
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-29 13:19:14 +0000

    9. Juggling async

--- program.js
+++ program.js
@@ -1,11 +1,17 @@
+"use strict";
 var http = require('http');
 var bl = require('bl');
 
-http.get(process.argv[2], function(response) {
-    response.setEncoding('utf8');
-    response.pipe(bl(function(err, data) {
-        if (err) return console.error(err);
-        console.log(data.length);
-        console.log(data.toString());
-    }));
-}).on('error', console.error);
+var strings = [];
+var count = 0;
+
+for (let i = 0; i < 3; i++) {
+    http.get(process.argv[i+2], function(response) {
+        response.pipe(bl(function(err, data) {
+            if (err) return console.error(err);
+            strings[i] = data.toString();
+            count++;
+            if (count == 3) strings.forEach(string => console.log(string));
+        }));
+    }).on('error', console.error);
+}
